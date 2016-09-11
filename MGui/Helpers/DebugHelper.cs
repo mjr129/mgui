d818110e9e1be65b2c1c840f3f989fd21b7996cd
mjr129 <mjr129@york.ac.uk>
2016-09-11 20:55:49 +0100

    Added DebugHelper

---
+++ MGui/Helpers/DebugHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGui.Helpers
+{
+    public static class DebugHelper
+    {
+        public static void NotifySlowFunction()
+        {
+        
+        }
+    }
+}
